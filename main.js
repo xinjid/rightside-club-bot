f37417bb74fb7a24a9ef3823f6f8eecff6a2540d
xinjid <your.xinjid7@gmail.com>
2026-02-20 00:25:36 +0300

    app: add electron entry files

---
+++ main.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 600,
+    title: "RightSide Admin Bot",
+  });
+
+  win.loadFile(path.join(__dirname, "index.html"));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
